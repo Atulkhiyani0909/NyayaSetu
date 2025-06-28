dbb3454e4a98e67d7256dceecb531ef9010f2ae2
Atul <atulkhiyani09@gmail.com>
2025-06-28 11:01:39 +0530

    Added Folder

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.static('public'))
+app.use(cookieParser())
+
+
+export default app 
+
+
